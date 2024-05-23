47286164a35f9d33c9bd5de9ec1f8929f554e234
angiehuan <ahuang@apps.district833.org>
2024-05-23 14:08:37 -0500

    Update FibonacciSequence.java

--- FibonacciSequence.java
+++ FibonacciSequence.java
@@ -1,8 +1,26 @@
 public class FibonacciSequence {
 
-    // precondition: num
+    // precondition: num > 0
     public String printSequence(int num) {
-        String result = "";
+        if (num = 1) {
+            return "1";
+        } else if (num = 2) {
+            return "1, 1";
+        } else {
+        String result = "1, 1, ";
+        int num1 = 1;
+        int num2 = 1;
+        for (int i = 0; i < num - 2; i++) {
+            if (i % 2 = 0) {
+                num1 = num1 + num2;
+                result = result + num1 + ", ";
+            } else {
+                num2 = num1 + num2;
+                result = result + num2 + ", ";
+            }
+        }
+        }
+        return result;
     }
 
 }
