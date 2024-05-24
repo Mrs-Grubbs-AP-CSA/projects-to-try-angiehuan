ad7ef12f132d4bb5c2006a0f3b78a5b7bb57b142
angiehuan <ahuang@apps.district833.org>
2024-05-24 10:06:57 -0500

    Update CollatzConjecture.java

--- CollatzConjecture.java
+++ CollatzConjecture.java
@@ -1,23 +1,21 @@
-public class CollatzConjecture {
-    public static int numSteps;
+import java.util.*;
 
-    public CollatzConjecture() {
-      numSteps = 0;
+public class CollatzConjecture {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Test Collatz's Conjecture by inputting a number: ");
+        int n = console.nextInt();
+        System.out.println("Steps: " + findNumSteps(n));
     }
 
     public int findNumSteps(int num) {
-        if (num == 0) {
-          return numSteps;
+        if (num == 1) {
+            return 0;
         } else if (num % 2 == 0) {         
-          numSteps++;
-          int newNum = n/2;
-          findNumSteps(newNum);
+            return 1 + findNumSteps(n/2);
         } else {
-          numSteps++;
-          int newNum = 3n+1;
-          findNumSteps(newNum);
+            return 1 + findNumSteps(3n+1);
         }
-      return numSteps;
     }
   
 }
