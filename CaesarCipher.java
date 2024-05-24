6c9d18ccab5bd48e6605c17ea13e4ef57f43f98e
angiehuan <ahuang@apps.district833.org>
2024-05-24 12:02:17 -0500

    Update CaesarCipher.java

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -4,11 +4,29 @@ public class CaesarCipher {
   public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.print("Encrypt the following message: ");
-        String n = console.nextString();
-        System.out.println("Encrypted message: " + shift(n));
+        String message = console.nextString();
+        System.out.print("Shifting number: ");
+        int number = console.nextInt();
+        System.out.println("Encrypted message: " + shift(message, number));
     }
 
-  public String shift(String msg) {
+  public String shift(String msg, int num) {
+        String[] array = new String[msg.length()];
+        int[] array2 = new int[msg.length()];
+        for (int i = 0; i < msg.length(); i++) {
+            array[i] = msg.substring(i, i+1);
+        }
+        for (int i = 0; i < msg.length(); i++) {
+            int n = convert(array[i]);
+            array2[i] = n;
+        }
+  }
+
+  public int convert(String letter) {
+    
+  }
+
+  public String convert2(int number) {
     
   }
   
