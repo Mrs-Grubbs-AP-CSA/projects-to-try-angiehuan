3e822ff7d6e96cf68ca16fe317ffdbd8d85ad038
angiehuan <ahuang@apps.district833.org>
2024-05-24 10:13:19 -0500

    Update FibonacciSequence.java

--- FibonacciSequence.java
+++ FibonacciSequence.java
@@ -1,6 +1,14 @@
-public class FibonacciSequence {
+import java.util.*;
 
-    // precondition: num > 0
+public class FibonacciSequence {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("List the Fibonacci Sequence up to your desired number of terms: ");
+        int n = console.nextInt();
+        System.out.println("Fibonnaci Sequence up to the " + n + "th term: " + printSequence(n));
+    }
+
+    // precondition: num is a positive integer
     public String printSequence(int num) {
         if (num = 1) {
             return "1";
