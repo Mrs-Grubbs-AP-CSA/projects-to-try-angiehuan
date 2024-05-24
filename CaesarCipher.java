d6a4c7c294a43d83dc9a8aa521f13956150dcac8
angiehuan <ahuang@apps.district833.org>
2024-05-24 10:25:58 -0500

    Update CaesarCipher.java

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -1,6 +1,15 @@
 import java.util.*;
 
 public class CaesarCipher {
-  
+  public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Encrypt the following message: ");
+        String n = console.nextString();
+        System.out.println("Encrypted message: " + shift(n));
+    }
+
+  public String shift(String msg) {
+    
+  }
   
 }
